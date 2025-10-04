30b50e167ceecd5922eb6c78aadfb426cc609760
Renan Henrique <120283287+Renhenq@users.noreply.github.com>
2025-10-04 11:29:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const button = document.getElementById('new-cat')
+const buttonCat = document.getElementById('new-cat')
+const buttonDog = document.getElementById('new-dog')
 const container = document.getElementById('cat-container')
 
 let img_gif = 0
@@ -6,7 +7,7 @@ var apiUrl
 
 async function newCat(){
     try{
-        button.disable = true;
+        buttonCat.disable = true;
 
         if (img_gif){
             apiUrl = 'https://api.thecatapi.com/v1/images/search?mime_types=gif'
@@ -33,8 +34,44 @@ async function newCat(){
     } catch(Error){
         console.log("Erro")
     } finally{
-        button.disable = false;
+        buttonCat.disable = false;
     }
 }
 
-button.addEventListener('click', newCat);
+buttonCat.addEventListener('click', newCat);
+
+async function newDog(){
+    try{
+        buttonDog.disable = true;
+
+        if (img_gif){
+            apiUrl = 'https://api.thedogapi.com/v1/images/search?mime_types=gif'
+        } else {
+            apiUrl = 'https://api.thedogapi.com/v1/images/search'
+        }
+
+        const response = await fetch(apiUrl);
+
+        if(!response.ok){
+            throw new Error("erro dentro")
+        }
+
+        const data = await response.json();
+        const imageUrl = data[0].url;
+
+        const img = document.createElement('img');
+        img.src = imageUrl;
+
+        container.appendChild(img)
+        img.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
+        img_gif = 1 - img_gif
+    } catch(Error){
+        console.log("Erro")
+    } finally{
+        buttonDog.disable = false;
+    }
+}
+
+
+buttonDog.addEventListener('click', newDog);
